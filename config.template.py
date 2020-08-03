37a2fd07a75b7d335c27016a48669456504e2102
peer35 <p.j.m.vos@vu.nl>
2020-08-03 09:34:15 +0200

    typo

--- config.template.py
+++ config.template.py
@@ -1,5 +1,5 @@
 FTP_USER = "geoplaza"
 FTP_PASSWORD = "**********"
 FTP_HOST = "geoplaza-ftp.iiifhosting.com"
-CSV_LIST = 'FreeUniversityAmterdam_maps.csv'
+CSV_LIST = 'FreeUniversityAmsterdam_maps.csv'
 FILENAME_COLUMN = 2
